9cc8f9d2f8b47a77d3f6fba63d426b4c4207ea5b
kaspermads <kaspergm@hotmail.no>
2024-08-05 12:13:45 +1000

    Updated the file

---
+++ COMP3702/A0_template copy.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Assignment 0 template
+
+For submission, rename this file to "A0.py" 
+
+Answer each question in the corresponding method definition stub below
+"""
+
+
+def Q1(A,B):
+    union = A | B
+    intersection = A & B
+    return union,intersection
+
+
+def Q2(A,B):
+    return 'DISJOINT'
+
+
+def Q3(a,b):
+    X = set()
+    Y = set()
+    G = set()
+    return X,Y,G
+
+
+def Q4(E,n):
+    n_successors = set()
+    return n_successors
+
+
+def Q5(inFile,outFile,remove):
+    print('Character '+remove+' removed from '+inFile)
+    print('Output written to '+outFile)
+
+
+def Q6(state1,state2):
+    print('IMPOSSIBLE')
+
+
+    Q1({1,2,3,4,5},{3,4,5,6,7})
